7ffa9dede716359c783597c2ffd2aaca2bfb9ffa
biruk-ay <birukindustry@gmail.com>
2025-03-23 20:00:34 +0300

    Changed login to hooks

--- src/apps/auth/presentation/view/Login.tsx
+++ src/apps/auth/presentation/view/Login.tsx
@@ -1,111 +1,84 @@
 import React from "react";
 import Header from "../../../../componets/header";
-import { login } from "../../application/slice/auth.slice";
-import { connect } from "react-redux";
+import { login, selectLoading, selectUserRole } from "../../application/slice/auth.slice";
 import Loading from "../../../../componets/loading";
 import Ticket from "../../../ticket/presentation/view/Tickets";
 import Admin from "../../../admin/presentation/view/Admin";
+import { useAppDispatch, useAppSelector } from "../../../../store/store";
 
-interface LoginProps {
-  role: string | null;
-  loading: boolean;
-  login: (data: { email: string; password: string }) => void;
-  
+interface FormElements extends HTMLFormControlsCollection {
+  emailInput: HTMLInputElement;
+  passwordInput: HTMLInputElement;
 }
 
-interface LoginState {
-  email: string;
-  password: string;
+interface LoginFormElements extends HTMLFormElement {
+  readonly elements: FormElements
 }
 
-export class Login extends React.Component<LoginProps, LoginState> {
-  state: LoginState = {
-    email: "",
-    password: "",
-  };
+const Login = () => {
+  const dispatch = useAppDispatch();
+  const role = useAppSelector(selectUserRole);
+  const loading = useAppSelector(selectLoading);
+  const handleSubmit = async (evt: React.FormEvent<LoginFormElements>) => {
+    evt.preventDefault();
 
-  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ [e.target.name]: e.target.value } as Pick<LoginState, keyof LoginState>);
-  };
-  handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    await this.props.login({
-      email: this.state.email,
-      password: this.state.password,
-    });
-  };
-  
-  componentDidUpdate(prevProps: LoginProps) {
-    if (prevProps.role !== this.props.role || prevProps.loading !== this.props.loading) {
+    const email = evt.currentTarget.elements.emailInput.value;
+    const password = evt.currentTarget.elements.passwordInput.value;
+    await dispatch(login({email: email, password: password}));
+  }
 
-      if (!this.props.loading) {
-        this.forceUpdate();
-      }
-    }
+  if(loading) {
+    return <Loading />
+  }
+
+  if (role === "user") {
+    return <Ticket />;
   }
   
-  render(): React.ReactNode {
-    const { role, loading } = this.props;
+  if (role === "admin") {
+    return <Admin />;
+  }
 
-    if (loading && (role===null)) {
-      return <Loading />;
-    }
-
-    if (role === "user") {
-      return <Ticket />;
-    }
-
-    if (role === "admin") {
-      return <Admin />;
-    }
-
-    return (
-      <>
-        <Header />
-        <div className="w-screen h-screen bg-white">
-          <div className="w-full h-full flex flex-col-reverse sm:flex-row items-center justify-evenly">
-            <form className="w-1/2 flex flex-col items-center justify-center" onSubmit={this.handleSubmit}>
-              <div className="flex text-black text-4xl italic font-pro-italic">Login</div>
-              <div className="text-black text-2xl font-pro mt-20 mr-56">Email</div>
-              <input
-                name="email"
-                type="email"
-                className="w-full sm:w-2/5 bg-[#D9D9D9] bg-opacity-50 rounded-lg py-3 pl-1 mt-2"
-                onChange={this.handleChange}
-              />
-              <div className="text-black text-2xl font-pro mt-6 mr-48">Password</div>
-              <input
-                name="password"
-                type="password"
-                className="w-full sm:w-2/5 bg-[#D9D9D9] bg-opacity-50 rounded-lg py-3 pl-1 mt-2"
-                onChange={this.handleChange}
-              />
-              <button
-                type="submit"
-                className="w-full sm:w-1/4 h-8 bg-primary text-white text-lg font-pro rounded-3xl mt-10"
-              >
-                Login
-              </button>
-            </form>
-            <div className="w-1/2 flex flex-col items-center justify-center">
-              <div className="font-cursive font-extrabold text-8xl text-primary">Tickets</div>
-              <div className="font-pro font-extrabold text-xl text-black">Get Your Tickets</div>
-            </div>
+  
+  return (
+    <>
+      <Header />
+      <div className="w-screen h-screen bg-white">
+        <div className="w-full h-full flex flex-col-reverse sm:flex-row items-center justify-evenly">
+          <form className="w-1/2 flex flex-col items-center justify-center" onSubmit={handleSubmit}>
+            <div className="flex text-black text-4xl italic font-pro-italic">Login</div>
+            <div className="text-black text-2xl font-pro mt-20 mr-56">Email</div>
+            <input
+              name="emailInput"
+              id="emailInput"
+              type="email"
+              className="w-full sm:w-2/5 bg-[#D9D9D9] bg-opacity-50 rounded-lg py-3 pl-1 mt-2"
+            />
+            <div className="text-black text-2xl font-pro mt-6 mr-48">Password</div>
+            <input
+              name="passwordInput"
+              id="passwordInput"
+              type="password"
+              className="w-full sm:w-2/5 bg-[#D9D9D9] bg-opacity-50 rounded-lg py-3 pl-1 mt-2"
+            />
+            <button
+              type="submit"
+              className="w-full sm:w-1/4 h-8 bg-primary text-white text-lg font-pro rounded-3xl mt-10"
+            >
+              Login
+            </button>
+          </form>
+          <div className="w-1/2 flex flex-col items-center justify-center">
+            <div className="font-cursive font-extrabold text-8xl text-primary">Tickets</div>
+            <div className="font-pro font-extrabold text-xl text-black">Get Your Tickets</div>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const mapStateToProps = (state: any) => ({
-  role: state.auth.role,
-  loading: state.auth.loading,
-});
+export default Login;
 
-const mapDispatchToProps = {
-  login,
-};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
