2afc3aeaa694f71719b3e72a3e417a0382dd71a0
biruk-ay <birukindustry@gmail.com>
2025-03-24 01:37:52 +0300

    Migrated signup to hooks

--- src/apps/auth/presentation/view/Signup.tsx
+++ src/apps/auth/presentation/view/Signup.tsx
@@ -1,120 +1,90 @@
-// @ts-nocheck
 import React from "react";
 import Header from "../../../../componets/header";
-import { signup } from "../../application/slice/auth.slice";
-import { connect } from "react-redux";
+import { selectLoading, selectUserRole, signup } from "../../application/slice/auth.slice";
 import Loading from "../../../../componets/loading";
 import Ticket from "../../../ticket/presentation/view/Tickets";
 import Admin from "../../../admin/presentation/view/Admin";
+import { useAppDispatch, useAppSelector } from "../../../../store/store";
 
-interface SignupProps {
-  role: string | null;
-  loading: boolean;
-  signup: (data: { name: string; email: string; password: string; role: string }) => void;
+
+interface FormElements extends HTMLFormControlsCollection {
+  nameInput: HTMLInputElement;
+  emailInput: HTMLInputElement;
+  passwordInput: HTMLInputElement;
+  roleInput: HTMLInputElement;
 }
 
-interface SignupState {
-  name: string;
-  email: string;
-  password: string;
-  role: string;
+interface SignupFormElements extends HTMLFormElement {
+  readonly elements: FormElements
 }
 
 
-export class Signup extends React.Component {
-  state: SignupState = {
-    name: "",
-    email: "",
-    password: "",
-    role: ""
-  };
 
-  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ [e.target.name]: e.target.value } as Pick<SignupState, keyof SignupState>);
-  };
+const Signup = () => {
 
-  handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault(); 
-    await this.props.signup({
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password,
-      role: this.state.role
-    });
-  };
-  
+  const dispatch = useAppDispatch();
+  const role = useAppSelector(selectUserRole);
+  const loading = useAppSelector(selectLoading);
 
-  componentDidUpdate(prevProps: SignupProps) {
-    if (prevProps.role !== this.props.role || prevProps.loading !== this.props.loading) {
-  
-      if (!this.props.loading) {
-        this.forceUpdate();
-      }
-    }
+  const handleSubmit = async (evt: React.FormEvent<SignupFormElements>) => {
+    evt.preventDefault();
+
+    const name = evt.currentTarget.elements.nameInput.value;
+    const email = evt.currentTarget.elements.emailInput.value;
+    const password = evt.currentTarget.elements.passwordInput.value;
+    const roleIn = evt.currentTarget.elements.roleInput.value;
+    console.log(name, email, password, roleIn);
+    await dispatch(signup({name: name, email: email, password: password, role: roleIn}))
+    
+  }
+
+  if (loading) {
+    return <Loading />;
   }
   
+  if (role === "user") {
+    return <Ticket />;
+  }
   
-  
-  
-  render(): React.ReactNode {
-    const { role, loading } = this.props;
-    if (loading) {
-      return <Loading />;
-    }
-
-    if (role === "user") {
-      return <Ticket />;
-    }
-
-    if (role === "admin") {
-      return <Admin />;
-    }
-    return (
-      <>
-        <Header />
-        <div className="w-screen h-screen bg-white">
-          <div className="w-full h-full flex flex-col-reverse sm:flex-row-reverse mt-8 sm:mt-0 items-center justify-evenly">
-            <form className="w-1/2 flex flex-col items-center justify-center" onSubmit={this.handleSubmit}>
-                <div className="flex text-black text-4xl italic font-pro-italic">
-                    Create Account
-                </div>
-                <div className="text-black text-2xl font-pro mt-20 mr-56">Name</div>
-                <input name="name" className="w-full sm:w-2/5 bg-[#D9D9D9] bg-opacity-50 rounded-lg py-3 pl-1 mt-2" onChange={this.handleChange} />
-                <div className="text-black text-2xl font-pro mt-6 mr-56">Email</div>
-                <input name="email" className="w-full sm:w-2/5 bg-[#D9D9D9] bg-opacity-50 rounded-lg py-3 pl-1 mt-2" onChange={this.handleChange}/>
-                <div className="text-black text-2xl font-pro mt-6 mr-48">Password</div>
-                <input name="password" className="w-full sm:w-2/5 bg-[#D9D9D9] bg-opacity-50 rounded-lg py-3 pl-1 mt-2" onChange={this.handleChange}/>
-                <div className="text-black text-2xl font-pro mt-6 mr-56">Role</div>
-                <div className="flex flex-row justify-evenly items-center">
-                    <div className="flex justify-evenly items-center">
-                        <input type="radio" id="user" name="role" value="user" className="focus:ring-primary focus:ring-2" onChange={this.handleChange}/>
-                        <label className="font-pro text-black text-lg mr-5 ml-1.5">User</label>
-                    </div>
-                    <div className="flex items-center">
-                        <input type="radio" id="admin" name="role" value="admin" className="focus:ring-primary focus:ring-2 ml-5" onChange={this.handleChange}/>
-                        <label className="font-pro text-black text-lg ml-1.5">Admin</label>
-                    </div>
-                </div>
-                <button type="submit" className="w-full sm:w-1/4 h-8 bg-primary text-white text-lg font-pro rounded-3xl mt-10">Create</button>
-            </form>
-            <div className="w-1/2 flex flex-col items-center justify-center">
-                <div className="font-cursive font-extrabold text-8xl text-primary">Tickets</div>
-                <div className="font-pro font-extrabold text-xl text-black">Get Your Tickets</div>
-            </div>
+  if (role === "admin") {
+    return <Admin />;
+  }
+  return (
+    <>
+      <Header />
+      <div className="w-screen h-screen bg-white">
+        <div className="w-full h-full flex flex-col-reverse sm:flex-row-reverse mt-8 sm:mt-0 items-center justify-evenly">
+          <form className="w-1/2 flex flex-col items-center justify-center" onSubmit={handleSubmit}>
+              <div className="flex text-black text-4xl italic font-pro-italic">
+                  Create Account
+              </div>
+              <div className="text-black text-2xl font-pro mt-20 mr-56">Name</div>
+              <input name="nameInput" id="nameInput" className="w-full sm:w-2/5 bg-[#D9D9D9] bg-opacity-50 rounded-lg py-3 pl-1 mt-2" />
+              <div className="text-black text-2xl font-pro mt-6 mr-56">Email</div>
+              <input name="emailInput" id="emailInput" className="w-full sm:w-2/5 bg-[#D9D9D9] bg-opacity-50 rounded-lg py-3 pl-1 mt-2" />
+              <div className="text-black text-2xl font-pro mt-6 mr-48">Password</div>
+              <input name="passwordInput" id="passwordInput" className="w-full sm:w-2/5 bg-[#D9D9D9] bg-opacity-50 rounded-lg py-3 pl-1 mt-2" />
+              <div className="text-black text-2xl font-pro mt-6 mr-56">Role</div>
+              <div className="flex flex-row justify-evenly items-center">
+                  <div className="flex justify-evenly items-center">
+                      <input type="radio" id="roleInput" name="role" value="user" className="focus:ring-primary focus:ring-2" />
+                      <label className="font-pro text-black text-lg mr-5 ml-1.5">User</label>
+                  </div>
+                  <div className="flex items-center">
+                      <input type="radio" id="roleInput" name="role" value="admin" className="focus:ring-primary focus:ring-2 ml-5" />
+                      <label className="font-pro text-black text-lg ml-1.5">Admin</label>
+                  </div>
+              </div>
+              <button type="submit" className="w-full sm:w-1/4 h-8 bg-primary text-white text-lg font-pro rounded-3xl mt-10">Create</button>
+          </form>
+          <div className="w-1/2 flex flex-col items-center justify-center">
+              <div className="font-cursive font-extrabold text-8xl text-primary">Tickets</div>
+              <div className="font-pro font-extrabold text-xl text-black">Get Your Tickets</div>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  role: state.auth.role,
-  loading: state.auth.loading,
-});
-
-const mapDispatchToProps = {
-  signup,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Signup);
+export default Signup;
