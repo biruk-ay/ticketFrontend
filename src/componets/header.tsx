fc2b9b96192fc0c02af7011da692b93cd8a33b45
biruk-ay <birukindustry@gmail.com>
2025-03-25 11:22:10 +0300

    Migrated header to hooks

--- src/componets/header.tsx
+++ src/componets/header.tsx
@@ -1,61 +1,51 @@
-// @ts-nocheck
 import React from "react";
-import { connect } from "react-redux";
-import { logout } from "../apps/auth/application/slice/auth.slice";
+import { logout, selectUserName } from "../apps/auth/application/slice/auth.slice";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { useNavigate } from "react-router";
 
-export class Header extends React.Component {
-    
-    handleLogout = () => {
-        this.props.logout();
-        window.location.href = "/login";
-      };
-
-    render(): React.ReactNode {
-        const { isLoggedIn, username } = this.props;
-
-        return (
-            <header className="flex bg-white w-full fixed top-0 left-0 p-3 box-border">
-                <div className="font-cursive font-extrabold text-2xl text-primary ml-16">Tickets</div>
-                <div className="ml-auto flex items-center space-x-4 mr-16">
-          {isLoggedIn ? (
-            <>
-              <span className="text-black font-pro">Welcome, {username}</span>
-              <button
-                onClick={this.handleLogout}
-                className="bg-primary font-pro text-white px-5 py-1 rounded-full transition hover:bg-green-900"
-                >
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <a
-                href="/login"
-                className="text-black font-pro hover:text-green-900"
-              >
-                Login
-              </a>
-              <a
-                href="/signup"
-                className="text-black font-pro hover:text-green-900"
-              >
-                Register
-              </a>
-            </>
-          )}
-        </div>
-            </header>
-        );
-    }
+const Header = () => {
+  
+  const dispatch = useAppDispatch();
+  const username = useAppSelector(selectUserName);
+  const navigator = useNavigate();
+  const handleLogout = async (evt: React.FormEvent) => {
+    evt.preventDefault();
+    await dispatch(logout());
+    navigator('/login')
+  }
+  return (
+      <header className="flex bg-white w-full fixed top-0 left-0 p-3 box-border">
+          <div className="font-cursive font-extrabold text-2xl text-primary ml-16">Tickets</div>
+          <div className="ml-auto flex items-center space-x-4 mr-16">
+    {username ? (
+      <>
+        <span className="text-black font-pro">Welcome, {username}</span>
+        <button
+          onClick={handleLogout}
+          className="bg-primary font-pro text-white px-5 py-1 rounded-full transition hover:bg-green-900"
+          >
+          Logout
+        </button>
+      </>
+    ) : (
+      <>
+        <a
+          href="/login"
+          className="text-black font-pro hover:text-green-900"
+        >
+          Login
+        </a>
+        <a
+          href="/signup"
+          className="text-black font-pro hover:text-green-900"
+        >
+          Register
+        </a>
+      </>
+    )}
+  </div>
+      </header>
+  );
 }
 
-const mapStateToProps = (state) => ({
-    isLoggedIn: state.auth.isLoggedIn, 
-    username: state.auth.name,
-  });
-  
-  const mapDispatchToProps = {
-    logout,
-  };
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default Header;
